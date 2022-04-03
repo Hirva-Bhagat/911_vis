0b13079826e99ae21a78c26913a6c78d4da30913
hirva <hirva@vt.edu>
2022-04-03 19:10:18 -0400

    parsed data

--- js/main.js
+++ js/main.js
@@ -5,44 +5,57 @@
 // init global variables, switches, helper functions
 let myCrimeClock,
     myDayNightScatter;
+let parseDate = d3.timeParse("%d/%m/%Y");
+let parseTime = d3.timeParse("%H:%M:%S");
 
 function updateAllVisualizations(){
     myCrimeClock.wrangleData()
     myDayNightScatter.wrangleData()
 }
 
-// load data
-d3.csv("data/911_clean.csv",function(data){
-    console.log(data);
-},function(error, rows){
-    console.log(rows);
-});
+d3.csv('data/911_clean.csv').then(function(d) {
+    data = d.filter(function(d){
+        if(isNaN(parseFloat(d.zip))){
+            return false;
+        }
+        else{
+            d.lat=d.lat,
+                d.lng=d.lng,
+                d.zip=parseInt(d.zip),
+                d.twp=d.twp,
+                d.addr=d.addr,
+                d.Time_of_call=parseTime(d.Time_of_call),
+                d.Date_of_call=parseDate(d.Date_of_call),
+                d.Type_of_call=d.Type_of_call,
+                d.Reason=d.Reason,
+                d.Time_of_day=d.Time_of_day,
+                d.Day_or_night=d.Day_or_night
+            return true;
+        }
 
-d3.csv('data/911_clean.csv')
-    .then(function(data) {
+    });
+
         initMainPage(data)
         console.log(data)
     })
     .catch(function(error){
         // handle error
-        console.log(err)
+        console.log(error)
     })
 
 
 // initMainPage
 function initMainPage(allDataArray) {
 
+    console.log("check here");
     // log data
     console.log(allDataArray);
 
     // activity 1, pie chart
-    myCrimeClock = new CrimeClock('div_name')
+    //myCrimeClock = new CrimeClock('div_name')
 
     // activity 2, force layout
-    myDayNightScatter = new DayNightVis('div_name', allDataArray[0], allDataArray[1])
+    //myDayNightScatter = new DayNightVis('div_name', allDataArray[0], allDataArray[1])
 
-    console.log("Check here");
 
-    console.log(allDataArray[0]);
-    console.log(allDataArray[1]);
 }
