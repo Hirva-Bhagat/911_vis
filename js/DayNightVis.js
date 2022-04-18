d16767da7bc65fc363bbbd2e5f77229d538b8f98
Hadi Rahmaninejad <hadirahmaninejad@Hadis-MacBook-Pro.local>
2022-04-18 17:55:32 -0400

    replacing a file

--- js/DayNightVis.js
+++ js/DayNightVis.js
@@ -41,10 +41,11 @@ class DayNightVis {
 
         // add title
         vis.svg.append('g')
-            .attr('class', 'title DNvis-title')
+            .attr('class', 'title bar-title')
             .append('text')
-            .text('types and counts of 911 calls')
+            .text('Types and Counts of 911 calls')
             .attr('transform', `translate(${vis.width / 2}, -15)`)
+            .attr("stroke","white")
             .attr('text-anchor', 'middle');
 
 
@@ -57,8 +58,8 @@ class DayNightVis {
 
         vis.groupData=d3.nest()
             .key(function(d){
-                return d.Day_or_night;
-            })
+            return d.Day_or_night;
+        })
             .key(function(d){
                 return d.Type_of_call;
             })
@@ -79,7 +80,7 @@ class DayNightVis {
         console.log(vis.xDomain)
 
         vis.yScale = d3.scaleLinear()
-            .domain([0,1300000000])
+            .domain([0,1500000000])
             .range([ vis.height, 0]);
         console.log(vis.yScale)
 
@@ -107,7 +108,8 @@ class DayNightVis {
             .style("text-anchor", "start")
             .attr("dx", "1em")
             .attr("dy", "0.5em")
-            .attr("transform", "rotate(-75)");
+            .style("color", "white")
+            .attr("transform", "rotate(-45)");
 
 
         vis.y = vis.svg.append("g")
@@ -128,12 +130,13 @@ class DayNightVis {
         vis.y.selectAll("text")
             .style("font", "10px sans-serif")
             .style("text-anchor", "end")
+            .style("color", "white")
             .attr("dx", "-1em")
             .attr("dy", "0.5em");
 
 
 
-        vis.wrangleData()
+vis.wrangleData()
     }
 
     // wrangleData method
@@ -156,7 +159,7 @@ class DayNightVis {
 
 
 
-        console.log(vis.groupData)
+console.log(vis.groupData)
         vis.updateVis()
 
     }
@@ -172,13 +175,14 @@ class DayNightVis {
             .data(vis.displayData)
             .enter().append("g")
             .attr("transform", function(d) {
-                return "translate(" + vis.xScale(d.DON) + "," + vis.yScale(d.count) + ")";
+                    return "translate(" + vis.xScale(d.DON) + "," + vis.yScale(d.count) + ")";
 
             });
 
+
         vis.bubble.append("circle")
             .attr("r", function(d) {
-                return vis.norm(d.count,500000,1300000000,4,20);
+                return vis.norm(d.count,500000,1500000000,4,20);
             })
             .style("fill", function(d) {
                 return d.colour})
@@ -186,6 +190,28 @@ class DayNightVis {
                 // d3.select(this)
                 $("#day-night-test").text("Count: " + d.count + " Type: " + d.TOC);
             });
+
+// add legend
+
+
+        let legendGroup = svg.append('g')
+            .attr('class', 'legend')
+            .attr('transform', 'translate(-20,50)');
+
+        let legendEntry = legendGroup.selectAll('g')
+            .data(this);
+        //create one legend entry for each series in the dataset array
+        //if that's not what you want, create an array that has one
+        //value for every entry you want in the legend
+
+        legendEntry.enter().append("g")
+            .attr("class", "legend-entry")
+            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+        //shift each entry down by approx 1 line (20px)
+
+        legendEntry.append("rect") //add a square to each entry
+        /* and so on */
+
 
     }
 }
